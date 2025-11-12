ca55691dd4dbc6db39e863bea5caaa29e675714f
Saniya Panhalkar <saniyapanhalkar2003@gmail.com>
2025-11-12 14:01:39 +0530

    Login,placements

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -57,27 +57,25 @@ const Login = () => {
  return (
     <div className="bg-transparent relative flex flex-col md:flex-row min-h-screen font-[Poppins] text-white items-center justify-center overflow-hidden">
 
-      <img
-        src="src\assets\saniya\bg.png"
-        alt="background"
-        className="absolute inset-0 w-full h-full object-cover z-0"
-      />
-
       <div className="flex flex-col md:flex-row items-center justify-center gap-10 md:gap-28 z-10 w-full relative px-4 md:px-12">
 
         <div className="relative w-full md:w-[450px] flex items-center justify-center">
-          <img
-            src="/src\assets\saniya\Log_In.png"
-            alt="login illustration"
-            className="w-full h-auto md:h-[500px] object-contain"
-          />
+
+         <h1
+            className="absolute left-1/2 -translate-x-1/2 md:left-10 md:translate-x-0 top-1/2 -translate-y-1/2 
+            text-[120px] md:text-[220px] font-extrabold 
+            text-[#c96b2c] opacity-40 
+            leading-none select-none tracking-tight pointer-events-none text-center md:text-left"
+          >
+            Log<br />In
+          </h1>
 
           <div className="absolute inset-0 flex items-center justify-center md:hidden">
             <div className="w-[90%] max-w-[380px] bg-transparent p-2 rounded-xl animate-popin">
               <LoginForm />
               <div className="text-sm text-gray-300 text-right mt-3 pr-1">
                 Don’t have an account?{" "}
-                <a href="/sign_up" className="text-orange-400 font-bold">
+                <a href="/Signup" className="text-orange-400 font-bold">
                   Sign Up
                 </a>
               </div>
