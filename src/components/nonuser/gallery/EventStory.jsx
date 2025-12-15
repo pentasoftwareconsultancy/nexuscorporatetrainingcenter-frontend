23eea061e02a86f3b0a02eea4cbc1dd57ca8aa85
Ajinkya Sasne <87075239+AJINKYASDEV@users.noreply.github.com>
2025-12-15 17:57:55 +0530

    Feature/ajinkya (#133)
    * redesign faculty, partner, VMV, Courses, FAQ, OurClient, Upcoming, Contact, Placement, PlacementStory
    
    * faculty, gallery, success, gallery info
    
    * cloned
    
    * changes solved
    
    * done
    
    * done
    
    * changes made
    
    * routes added
    
    * fixed error
    
    * display error fixed
    
    * pages added
    
    * some changes made
    
    * error solved
    
    * changes made
    
    * changes done
    
    * changes made in home hero
    
    * pulled
    
    * error solved
    
    * backend routing working in progress
    
    * changes made
    
    * changes
    
    * routeing error solved
    
    * working on backend to frontend connection
    
    * error resolved
    
    * cnahged made
    
    * changes made
    
    * merged
    
    * merged
    
    * changes made
    
    * changes made
    
    * changes made
    
    * changes made
    
    * changes made
    
    * changes made

--- src/components/nonuser/gallery/EventStory.jsx
+++ src/components/nonuser/gallery/EventStory.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { X } from "lucide-react";
 import CoursesCard from "../../common/StoryCard";
 import eventData from "../../../assets/vaishnavi/eventStoryData.json";
+import images from "../../../assets/shubham/abhijeet.png";
 
 export default function EventStory() {
   const mainScrollRef = useRef(null);
@@ -34,32 +35,59 @@ export default function EventStory() {
       {/* ✅ Title */}
       <h2 className="text-start text-2xl sm:text-3xl md:text-5xl font-extrabold mb-8 sm:mb-8 tracking-wide text-white  ">
         Event Stories
-        
       </h2>
       <p className="text-white pb-8 ">
-        Immerse yourself in our world of collaboration and innovation. Our gallery showcases the dynamic events, expert-led workshops, and successful placement drives <br></br>conducted in partnership with leading colleges and industry clients. This is where learning meets real-world application, capturing the energy of transformative<br></br> professional development.
+        Immerse yourself in our world of collaboration and innovation. Our
+        gallery showcases the dynamic events, expert-led workshops, and
+        successful placement drives <br></br>conducted in partnership with
+        leading colleges and industry clients. This is where learning meets
+        real-world application, capturing the energy of transformative<br></br>{" "}
+        professional development.
       </p>
       <div className="relative w-full flex items-center justify-center  transition-all duration-500 grayscale hover:grayscale-0">
-
-      {/* ✅ Auto-Scrolling Gallery */}
-      <div
-        ref={mainScrollRef}
-        className="w-full overflow-x-hidden whitespace-nowrap pb-6 "
-        onMouseEnter={() => (isMainPaused.current = true)}
-        onMouseLeave={() => (isMainPaused.current = false)}
+        {/* ✅ Auto-Scrolling Gallery */}
+        <div
+          ref={mainScrollRef}
+          className="w-full overflow-x-hidden whitespace-nowrap pb-6 "
+          onMouseEnter={() => (isMainPaused.current = true)}
+          onMouseLeave={() => (isMainPaused.current = false)}
         >
-        <div className="inline-flex gap-4 sm:gap-6 px-2 sm:px-4">
-          {[...eventData, ...eventData].map((event, index) => (
+          <div className="inline-flex gap-4 sm:gap-6 px-2 sm:px-4">
+            <div
+              className="relative flex flex-col justify-between rounded-2xl cursor-pointer
+                overflow-hidden border-2 min-w-[280px] sm:min-w-[240px] md:min-w-[260px] text-white transition-all 
+                duration-300 ease-in-out hover:scale-[1.02]group"
+            >
+              <div className="w-full h-48 overflow-hidden">
+                <img 
+                  src={images} 
+                  alt={images} 
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105" 
+                />
+              </div>
+              <div>
+                <p>
+                  Event Name - <span>MR. AB</span>
+                </p>
+                <p>
+                  Date - <span>2 Octomber 2025</span>
+                </p>
+                <p>
+                  Location - <span>Pune</span>
+                </p>
+              </div>
+            </div>
+            {/* {[...eventData, ...eventData].map((event, index) => (
             <CoursesCard
             key={index}
             image={`/src/assets/vaishnavi/${event.image}`}
             title={event.title}
             description={event.description}
             />
-          ))}
+          ))} */}
+          </div>
         </div>
       </div>
-          </div>
     </div>
   );
 }
