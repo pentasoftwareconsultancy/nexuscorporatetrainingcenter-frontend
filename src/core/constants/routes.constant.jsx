aa3864521e8b7526076c90c57766bcd559070463
Ajinkya Sasne <ajinkya.sasne@gmail.com>
2025-12-17 17:38:28 +0530

    changes made

--- src/core/constants/routes.constant.jsx
+++ src/core/constants/routes.constant.jsx
@@ -34,14 +34,18 @@ export const ROUTES = {
   ADMIN_COLLEGE_VISIT: "/visitdashboard",
   ADMIN_TOTAL_PLACEMENTS: "/placementdashboard",
   ADMIN_REVIEWS: "/reviewsdashboard",
+  //Gallery
   ADMIN_GALLERY_EVENT: "/galleryevent",
+  ADMIN_EVENT_DETAIL_ADD: "/eventdetail/add",
+  ADMIN_EVENT_DETAIL_EDIT: "/eventdetail/edit/:id",
+  //Courses
   ADMIN_COURSES: "/admincourses",
-  ADMIN_NOTIFICATION: "/adminnotification",
-  ADMIN_PROFILE: "/adminprofile",
   ADMIN_ADD_COURSE: "/addcourses",
   ADMIN_ADD_COURSE_WITH_ID: "/addcourses/:id",
+  //
+  ADMIN_NOTIFICATION: "/adminnotification",
+  ADMIN_PROFILE: "/adminprofile",
   ADMIN_VISIT_DETAIL: "/visitdetail",
-  ADMIN_EVENT_DETAIL: "/eventdetail",
   ADMIN_PLACEMENT_DETAIL: "/placementdetail",
   ADMIN_REGISTER_DETAIL: "/registerdetail",
   ADMIN_TEST_COMPLETE_DETAIL: "/completedetail",
